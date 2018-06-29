fb75d56be47ea217db7488390f9ac494ec149924
kaso <kaso@kasos-MacBook-Pro.local>
2018-06-29 13:02:30 +0800

    fuck koa request reponse

--- routes/controller/user.contorller.js
+++ routes/controller/user.contorller.js
@@ -2,7 +2,8 @@ const userModel = require('../../models/user')
 const fs = require('fs')
 
 exports.Signup = ctx => {
-    console.log(ctx.body)
+    console.log(ctx.request.body)
+    ctx.response.status = 200
 }
 
 exports.Render = ctx => {
